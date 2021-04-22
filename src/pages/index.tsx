4243a87df5623c838903cf01d267cbb8ef1ac72f
Neder <nedersimoes@gmail.com>
2021-04-22 15:55:17 -0400

    aula 4

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,13 @@
-import { GetStaticProps } from "next";
 import Image from "next/image";
 import Link from "next/link";
+import { GetStaticProps } from "next";
+
 import { format, parseISO } from "date-fns";
 import ptBR from "date-fns/locale/pt-BR";
+
 import { api } from "../services/api";
+import { useContext } from "react";
+import { PlayerContext } from "../contexts/PlayerContext";
 import { convertDurationToTimeString } from "../utils/convertDurationToTimeString";
 
 import styles from "./home.module.scss";
@@ -25,6 +29,8 @@ type HomeProps = {
 };
 
 export default function Home({ latestEpisodes, allEpisodes }: HomeProps) {
+  const { play } = useContext(PlayerContext);
+
   return (
     <div className={styles.homepage}>
       <section className={styles.latestEpisodes}>
@@ -50,7 +56,7 @@ export default function Home({ latestEpisodes, allEpisodes }: HomeProps) {
                   <span>{episode.durationAsString}</span>
                 </div>
 
-                <button type="button">
+                <button type="button" onClick={()=> play(episode)}>
                   <img src="/play-green.svg" alt="Tocar episódio" />
                 </button>
               </li>
